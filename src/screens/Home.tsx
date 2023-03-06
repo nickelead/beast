45d937d48c14671584cb2c7a022b2d84375b22eb
nickelead <59025216+nickelead@users.noreply.github.com>
2023-03-07 02:02:22 +0300

    feat(assets): Add images

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { ImageBackground, StyleSheet } from 'react-native';
 import { MoodPicker } from '../components';
 import { useAppContext } from '../App.provider';
+
+const imageUrl =
+  'https://images.unsplash.com/photo-1474540412665-1cdae210ae6b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1766&q=80';
 
 export const Home = () => {
   const { handleAddEntry } = useAppContext();
 
   return (
-    <View style={styles.container}>
+    <ImageBackground source={{ uri: imageUrl }} style={styles.container}>
       <MoodPicker onSelect={handleAddEntry} />
-    </View>
+    </ImageBackground>
   );
 };
 
