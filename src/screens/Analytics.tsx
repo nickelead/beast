50e5ad1f4b14fd7313fc58cfce027be333272b99
nickelead <59025216+nickelead@users.noreply.github.com>
2023-03-19 01:42:34 +0300

    feat(analytics): Add pie chart for moods

--- src/screens/Analytics.tsx
+++ src/screens/Analytics.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useMemo } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { useAppContext } from '../App.provider';
+import { VictoryPie } from 'victory-native';
+import { theme } from '../constants';
 
-type IProps = {};
+export const Analytics = () => {
+  const { moodList } = useAppContext();
 
-export const Analytics = ({}: IProps) => {
+  const data = useMemo(() => {
+    const options = moodList.reduce((acc, item) => {
+      if (acc[item.mood.emoji] !== undefined) {
+        return {
+          ...acc,
+          [item.mood.emoji]: acc[item.mood.emoji] + 1,
+        };
+      }
+      return {
+        ...acc,
+        [item.mood.emoji]: 1,
+      };
+    }, {} as Record<string, number>);
+
+    return Object.entries(options)
+      .map(([key, value]) => ({ x: key, y: value }))
+      .filter(({ y }) => y);
+  }, [moodList]);
+
   return (
     <View style={styles.container}>
-      <Text>Analytics</Text>
+      <VictoryPie
+        data={data}
+        labelRadius={80}
+        radius={150}
+        innerRadius={50}
+        style={{ labels: { fontSize: 30 } }}
+        colorScale={[
+          theme.colorPurple,
+          theme.colorLavender,
+          theme.colorBlue,
+          theme.colorGrey,
+          theme.colorWhite,
+        ]}
+      />
     </View>
   );
 };
@@ -14,5 +49,7 @@ export const Analytics = ({}: IProps) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
